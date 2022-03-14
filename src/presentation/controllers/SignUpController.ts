cbd00341ea5e2ab7f6302597d289e75b19c7f1b1
Ryan Ribeiro <me@ryanribeiro.dev>
2022-03-13 22:44:12 -0300

    refactor: add MissingParamError

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,18 +1,19 @@
 import { HttpRequest } from '../protocols/HttpRequest'
 import { HttpResponse } from '../protocols/HttpResponse'
+import { MissingParamError } from '../errors/MissingParamError'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MissingParamError('name')
       }
     }
 
     return {
       statusCode: 400,
-      body: new Error('Missing param: email')
+      body: new MissingParamError('email')
     }
   }
 }
