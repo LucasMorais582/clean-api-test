2c29062aa017c01c5d00f59dcd5941385d6a0aef
Ryan Ribeiro <me@ryanribeiro.dev>
2022-03-13 22:40:34 -0300

    refactor: add HttpRequest and HttpResponse interfaces

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,5 +1,8 @@
+import { HttpRequest } from '../protocols/HttpRequest'
+import { HttpResponse } from '../protocols/HttpResponse'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
@@ -7,11 +10,9 @@ export class SignUpController {
       }
     }
 
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
-      }
+    return {
+      statusCode: 400,
+      body: new Error('Missing param: email')
     }
   }
 }
