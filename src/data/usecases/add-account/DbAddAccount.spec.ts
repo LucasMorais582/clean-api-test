78f40c2244b66daddf9110edc9477057e099b5be
Ryan Ribeiro <me@ryanribeiro.dev>
2022-04-02 22:21:54 -0300

    refactor: move sut and encrypter creation to a helper method

--- src/data/usecases/add-account/DbAddAccount.spec.ts
+++ src/data/usecases/add-account/DbAddAccount.spec.ts
@@ -1,17 +1,34 @@
 import { DbAddAccount } from './DbAddAccount'
+import { Encrypter } from '../../protocols/Encrypter'
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await Promise.resolve('hashed_password')
+    }
+  }
+
+  return new EncrypterStub()
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return { sut, encrypterStub }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypt with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return await Promise.resolve('hashed_password')
-      }
-    }
+    const { sut, encrypterStub } = makeSut()
 
-    const encrypterStub = new EncrypterStub()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
-    const sut = new DbAddAccount(encrypterStub)
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
